70e631551f709d51bca7d79da38cd7ce80061081
Tyler Browder <tbrowder1@gmail.com>
2014-07-26 14:20:03 -0500

    Image Upload with Paperclip

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'jbuilder', '~> 2.0'
 
 gem 'bootstrap-sass'
 gem 'devise', '~>3.2.4'
+gem 'paperclip', '~> 4.0'
 
 
 group :development, :test do
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -24,7 +24,6 @@ class PinsController < ApplicationController
   
   def create
     @pin = current_user.pins.build(pin_params)
-
       if @pin.save
         redirect_to @pin, notice: 'Pin was successfully created.' 
       else
